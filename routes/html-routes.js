f7ef7e718ec88749ef6216ab3eb1be2408c0fad6
kytaylor <ky.taylor5468@gmail.com>
2021-05-26 16:21:53 -0700

    adds base code for calendar page, adjusts minor styling for task cards

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/html/index.html"))
 });
 
+router.get("/calendar", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/calendar.html"))
+});
+
 module.exports = router;
