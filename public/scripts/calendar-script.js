c55dc54e7eed3625b8ffa0fe13442b32537fe5ca
kytaylor <ky.taylor5468@gmail.com>
2021-06-13 17:24:59 -0700

    adds code to make calendar render with days properly positioned based on the current month

--- public/scripts/calendar-script.js
+++ public/scripts/calendar-script.js
@@ -1,7 +1,27 @@
+// Code for finding current date
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+// Finding first day of month
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
+let firstDay = (new Date(currentYear, currentMonth)).getDay();
+let daysInMonth = 32 - new Date(currentYear, currentMonth, 32).getDate();
+
+// -----------------
+today = mm + '/' + dd + '/' + yyyy;
+$("#month-year").append(today);
+console.log(today)
+
 const calendar = document.querySelector("#calendar-container")
 
-for (let day = 1; day <= 31; day++) {
-    console.log(day)
+for (let index = 0; index < firstDay; index++) {
+    calendar.insertAdjacentHTML("beforeend", `<div class="day"></div>`);
+}
 
+for (let day = 1; day <= daysInMonth; day++) {
     calendar.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
 }
